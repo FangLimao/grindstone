2c17a72f9345aaf7307fbe787a307368fd351124
方琉璃 <mucigames@outlook.com>
2025-01-09 13:56:22 +0800

    feat:添加了三个array

--- components/grindstone-common/src/index.ts
+++ components/grindstone-common/src/index.ts
@@ -1,3 +1,4 @@
 export * from "./lib/kit";
 export * from "./lib/effect";
 export * from "./lib/error"
+export * from "./lib/block";
---
+++ components/grindstone-common/src/lib/block.ts
@@ -0,0 +1,49 @@
+/**
+ * All block ids that shovel can convert.
+ */
+export const shovelConvertBlockIds: string[] = [
+  "grass_block",
+  "dirt",
+  "coarse_dirt",
+  "mycelium",
+  "podzol",
+  "dirt_with_roots",
+];
+
+/**
+ * All block ids that axe can convert.
+ */
+export const axeConvertBlockIds: string[] = [
+  "oak_wood",
+  "spruce_wood",
+  "birch_wood",
+  "jungle_wood",
+  "acacia_wood",
+  "dark_oak_wood",
+  "mangrove_wood",
+  "cherry_wood",
+  "crimson_hyphae",
+  "warped_hyphae",
+  "oak_log",
+  "spruce_log",
+  "birch_log",
+  "jungle_log",
+  "acacia_log",
+  "dark_oak_log",
+  "mangrove_log",
+  "cherry_log",
+  "crimson_stem",
+  "warped_stem",
+  "bamboo_block",
+];
+
+/**
+ * All block ids that hoe can convert.
+ */
+export const hoeConvertBlockIds: string[] = [
+  "dirt",
+  "grass_block",
+  "grass_path",
+  "coarse_dirt",
+  "dirt_with_roots",
+];
