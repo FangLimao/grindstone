9357c2cd3b6da69ea6577fdd598712ba2af05483
方琉璃 <mucigames@outlook.com>
2025-02-21 16:55:16 +0800

    v1.0.0-a3（0.17.1）

--- grindstone-mc/src/index.ts
+++ grindstone-mc/src/index.ts
@@ -1,3 +1,5 @@
 export * from "@grindstone/core";
+export * from "@grindstone/utils";
+export * from "@grindstone/common";
 export * from "@grindstone/format-kit";
 export * as formatKit from "@grindstone/format-kit";
